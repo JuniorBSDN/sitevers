60dd541c96fd6ec4e921513cc21178d1f71c422b
Airton JR <123423577+JuniorBSDN@users.noreply.github.com>
2025-07-09 14:32:39 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,28 +1,3 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import firebase_admin
-from firebase_admin import credentials, firestore
-import os
-import json
-
-app = Flask(__name__)
-CORS(app)
-
-# Firebase com variável de ambiente
-if not firebase_admin._apps:
-    try:
-        firebase_json = os.environ.get("FIREDB")
-        cred_dict = json.loads(firebase_json)
-        cred = credentials.Certificate(cred_dict)
-        firebase_admin.initialize_app(cred)
-        db = firestore.client()
-    except Exception as e:
-        print(f"Erro ao inicializar Firebase: {e}")
-        db = None
-else:
-    db = firestore.client()
-
-
 @app.route("/api/", methods=["POST"])
 def salvar_contato():
     if not db:
@@ -33,7 +8,7 @@ def salvar_contato():
         if not all(dados.get(k) for k in ("nome", "email", "assunto")):
             return jsonify({"erro": "Campos obrigatórios faltando."}), 400
 
-        db.collection("contatos").add({
+        db.collection("FIREDB").add({  # <- Aqui é onde foi alterado
             "nome": dados["nome"],
             "email": dados["email"],
             "assunto": dados["assunto"],
